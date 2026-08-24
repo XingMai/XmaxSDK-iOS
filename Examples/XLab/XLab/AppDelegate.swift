3da089f69300b7afb2e75455a4e228030b7a4f5e
WengYuehTing <WengYuehTing@163.com>
2026-08-24 20:39:28 +0800

    fix: adopt scene lifecycle in XLab

--- Examples/XLab/XLab/AppDelegate.swift
+++ Examples/XLab/XLab/AppDelegate.swift
@@ -1,22 +1,23 @@
 import UIKit
-import XmaxSDK
 
 @main
 final class AppDelegate: UIResponder, UIApplicationDelegate {
-
-    var window: UIWindow?
 
     func application(
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
-        let window = UIWindow(frame: UIScreen.main.bounds)
-        window.rootViewController = UINavigationController(
-            rootViewController: ViewController()
-        )
-        window.makeKeyAndVisible()
-        self.window = window
-
         return true
+    }
+
+    func application(
+        _ application: UIApplication,
+        configurationForConnecting connectingSceneSession: UISceneSession,
+        options: UIScene.ConnectionOptions
+    ) -> UISceneConfiguration {
+        UISceneConfiguration(
+            name: "Default Configuration",
+            sessionRole: connectingSceneSession.role
+        )
     }
 }
